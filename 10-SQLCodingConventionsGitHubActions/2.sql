89eda999eef283e0f4839e7ca15db10d3abbe005
Anna_Bondarenko <77499662+Ann-Bond-coder@users.noreply.github.com>
2022-12-19 17:30:13 +0300

    Update 2.sql

--- 10-SQLCodingConventionsGitHubActions/2.sql
+++ 10-SQLCodingConventionsGitHubActions/2.sql
@@ -3,16 +3,13 @@ CREATE TABLE Services(
     Serv_name VARCHAR(15) -- название услуги
 );
 
-
-CREATE TABLE Implementers(
 /*
 L014	7 / 2	Unquoted identifiers must be consistently capitalised.
-*/
-    Implem_id NUMBER(6), -- номер сотрудника
-    Implem_name VARCHAR(10), -- имя сотрудника
-/*
 L008	9 / 17	Commas should be followed by a single whitespace unless followed by a comment.
 */
+CREATE TABLE Implementers(
+    Implem_id NUMBER(6), -- номер сотрудника
+    Implem_name VARCHAR(10), -- имя сотрудника
     Salary NUMBER(7, 2) --зарплата сотрудника
 );
 
@@ -26,15 +23,13 @@ CREATE TABLE List_of_services(
     Service_implementer NUMBER(6) -- номер исполняющего сотрудника
 );
 
-CREATE TABLE Registration(
-    Reg_id NUMBER(6), -- номер записи
 /*
 L014	21 / 2	Unquoted identifiers must be consistently capitalised.
-*/
-    Reg_client VARCHAR(25), -- номер клиента
-    Service_for_registration NUMBER(6), -- название услуги
-/*
 L014	23 / 2	Unquoted identifiers must be consistently capitalised.
 */
+CREATE TABLE Registration(
+    Reg_id NUMBER(6), -- номер записи
+    Reg_client VARCHAR(25), -- номер клиента
+    Service_for_registration NUMBER(6), -- название услуги
     Reg_date DATE -- дата выполнения услуги
 );
